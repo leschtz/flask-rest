8c4fa610b1f6d4b521c1f548a4242767f5d9d795
Lukas Schuetz <leschtz@icloud.com>
2021-07-14 21:39:37 +0200

    /users returns now a full list with all users and displays it on the webpage, when clicking the button

--- controllers/UserController.py
+++ controllers/UserController.py
@@ -1,12 +1,12 @@
-from flask import render_template, redirect, url_for, request, abort
+from flask import json, render_template, jsonify, Response
+from bson import json_util
+
 from models.User import User
 from config import MONGO_DB
 
 
 def index():
-    for x in MONGO_DB.db.users.find():
-        print(x)
-    return render_template("pass.html")
+    return jsonify(json_util.dumps(MONGO_DB.db.users.find()))
 
 
 def store():
